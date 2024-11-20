9968bdfeb28b841015eaf09979555ae2f63de858
Ion Gireada <shytiger@yahoo.com>
2024-11-20 21:55:57 +0200

    update 1 file and delete 8 files

--- index.js
+++ index.js
@@ -1,87 +1,46 @@
 #!/usr/bin/env node
 
-'use strict';
-
-const { Command } = require('commander');
+const {Command} = require('commander')
 const path = require('path');
-const fs = require('fs');
-const yamljs = require('yamljs');
-const { normalizeItem } = require('./lib/skelo-utils');
-const { checkPatterns } = require('./lib/skelo-utils');
-const { buildItems } = require('./lib/skelo-utils');
 
-const program = new Command();
+// TODO: Update description property in package: refer to v2 and up of Docusaurus
 
-const { name, description, version } = require('../skelosaurus/package.json');
-const { validateFiles, processFiles, sendMessage } = require('./lib/skelo-utils');
-const fallbackPatterns = [['**/*.[oO]utline.yaml'], ['__outlines__/**/*.yaml']];
+const {name, description, version} = require('./package.json')
+let program = new Command();
 
 program
     .name(name)
-    .description(description)
     .version(version)
+    .description(description)
 
 program
-    .command('check')
-    .alias('c')
-    .description('check if files are valid')
-    .argument('[patterns...]', 'Glob patterns to match files', [])
-    .option('-s, --schema <path>', 'path to schema', path.join(__dirname, 'outline.schema.json'))
-    .option('-v, --verbose', 'verbose output')
-    .action((patterns, options) => {
-        checkPatterns(patterns, options, fallbackPatterns);
-    })
-
-program
-    .command('build', { isDefault: true })
+    .command('build', {isDefault: true})
     .alias('b')
-    .description('build doc files and sidebars file')
-    .argument('[patterns...]', 'Glob patterns to match files', [])
+    .description('build documentation files')
+    .argument('[patterns...]', 'outline file patterns')
 
-    .option('-d, --docs <path>', 'path where markdown files are generated into', './docs')
-    .option('-s, --sidebars <filepath>', 'path where sidebars file is generated into', './sidebars.js')
-    .option('-t, --templates <path>', 'path to templates', path.join(__dirname, 'templates'))
-    .option('--schema <path>', 'path to schema', path.join(__dirname, 'outline.schema.json'))
-    .option('-v, --verbose', 'verbose output')
+    .option('-d, --docs <path>', 'path to folder of generated documentation files', './docs')
+    .option('-s, --sidebars <filepath>', 'path and filename to generated sidebars layout', './sidebars.js')
+    .option('--verbose', 'verbose output')
+    .option('-t, --templates <path>', 'path to template files', path.join(__dirname, 'templates'))
+    .option('--schema <filepath>', 'path and filename to schema file', path.join(__dirname, 'schemas', 'outline.schema.json'))
+    .option('--fallback-patterns <patterns...>', 'fallback outline file patterns', ['**/*.[Oo]utline.(yaml|yml)', '__outlines__/**/*.(yaml|yml)'])
 
     .action((patterns, options) => {
+        console.log("🚀 ~ .action ~ patterns:", patterns)
+        const {fallbackPatterns} = options;
+        console.log("🚀 ~ .action ~ fallbackPatterns:", fallbackPatterns)
 
-        const diagnostics = checkPatterns(patterns, options, fallbackPatterns);
-        const duplicatedSidebarNames = diagnostics.duplicatedSidebarNames ? Object.keys(diagnostics.duplicatedSidebarNames).sort() : [];
-        const validFiles = diagnostics.valid || [];
-
-        if (duplicatedSidebarNames.length > 0) {
-            sendMessage(true, 'skelosaurus', 'warning', `Duplicated sidebar names: ${duplicatedSidebarNames.join(', ')} will not be generated as it's already in files: ${JSON.stringify(Object.values(diagnostics.duplicatedSidebarNames).sort(), null, 4)}`);
-        }
-    
-        const documentationSidebar = {};
-
-        validFiles.forEach(file => {
-            const yaml = yamljs.load(file);
-            const { sidebars: items = [], ...rest } = yaml;
-            const { path: parentPath } = rest;
-
-            const normalizedItems = items
-                .map(normalizeItem)
-                .filter(sidebar => !duplicatedSidebarNames.includes(sidebar.label));
-
-                console.log("🚀 ~ .action ~ parentPath:", parentPath)
-            normalizedItems.forEach(sidebar => {
-                documentationSidebar[sidebar.label] = buildItems(sidebar.items, {
-                    ...options,
-                    parentPath
-                });
-            });
-        }); 
-            
-
-        console.log("🚀 ~ .action ~ documentationSidebar:", documentationSidebar)
-
-
+        
     })
 
-program.parse("node index.js build --verbose".split(" "));
-// program.parse("node index.js check sample.outline.yaml sa*.outline.yaml".split(" "));
-// program.parse();
+
+program.configureHelp({
+    sortOptions: true,
+    sortSubcommands: true,
+    helpWidth: 80,
+    showGlobalOptions: true
+})
 
 
+program.parse()
