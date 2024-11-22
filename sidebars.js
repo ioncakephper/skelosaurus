7cc65fc059ed96beb66f9b2f5123a48794941e57
Ion Gireada <shytiger@yahoo.com>
2024-11-22 21:18:10 +0200

    * fix(skelo-utils.js): filter out null, undefined, and empty string items in buildSidebar and buildCategory functions * chore(skelo-utils.js): add TODO comment to generate unit tests for buildTopic function * feat(skelo-utils.js): add createTopicDocument function to generate topic documents in Markdown * chore(sidebars.js): remove unnecessary doc objects and use plain strings for item-3 and item-4

--- sidebars.js
+++ sidebars.js
@@ -1,14 +1,6 @@
 module.exports = {
     "Sidebar 2": [
-        {
-            "type": "doc",
-            "id": "./item-3",
-            "label": "Item 3"
-        },
-        {
-            "type": "doc",
-            "id": "./item-4",
-            "label": "Item 4"
-        }
+        "item-3",
+        "item-4"
     ]
 };
